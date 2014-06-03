643052d9ea21112d8f19fc4f5fbbbd21a8bb0d4b
Craig Hunter <craig@craighunter.ca>
2014-06-03 16:39:05 -0400

    Exercise 6: shopping_list Modified

--- shopping_list.rb
+++ shopping_list.rb
@@ -1,7 +1,19 @@
 grocery_list = ["tuna", "chicken", "protein", "eggs"]
 count = 0
+grocery_list << "rice"
 
-while count < 4
+while count < 5
 	puts "* #{grocery_list[count]}"
 	count +=1
 end
+
+puts "Total items to grab: #{grocery_list.length}"
+
+grocery_list.include?("bananas")
+	if true
+		puts "You don't need to pick up bananas!"
+	else
+		puts "You need to pick up bananas!"
+	end
+
+print "#{grocery_list[1]}"
