0f62fa701bd68573e0ea9d36dba2e073494021bc
Craig Hunter <craig@craighunter.ca>
2014-06-03 15:56:09 -0400

    Ruby Fundamentals 2: Exercise 5 Completed

---
+++ exercise5.rb
@@ -0,0 +1,9 @@
+p "What temperature (in Fahrenheit) would you like to convert to Celsius?"
+f = gets.chomp
+f = f.to_i
+
+def celsius_convert(f)
+ return ((f - 32) * 5/9)
+end
+
+puts celsius_convert(f)
